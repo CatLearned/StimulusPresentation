c6c0ccef92c641a0ddb11fe260806597a5dd916d
CatLearned <vovanvinni@gmail.com>
2020-06-27 14:37:08 +0300

    Добавлена запись временных меток и выход из цикла (Из цикла выходит, но процесс висит). Эксперимент в файле .txt

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from psychopy import core, visual, event
+from psychopy import core, visual
+from psychopy import prefs
+from psychopy.hardware import keyboard
 from p300_socket_class import p300_socket
+import time
 
-print("Test change")
 
 def number_to_direction(num):
     if num == 0:
@@ -17,6 +19,11 @@ def number_to_direction(num):
     else:
         return "Error"
 
+
+# Инициализация клавиатуры, для проверки "ESC"
+defaultKeyboard = keyboard.Keyboard()
+# Файл для записи эксперимента
+f = open('ExperimentStimulusPresentation.txt', 'w')
 # Количество секунд между демонстрациями
 duration = 1
 # Тип вывода
@@ -54,38 +61,52 @@ actual_stimulus_num = 0
 p300_info.start_receive()
 
 # Отображение и работа со стимулами производится в цикле
-while 1:
-    # Получаем текущее время
-    t = trialClock.getTime()
-    tThisFlip = window.getFutureFlipTime(clock=trialClock)
-    tThisFlipGlobal = window.getFutureFlipTime(clock=None)
-    frameN = frameN + 1  # Количество отрисованных кадров
-    # print(t, frameN)
-    if t - last_action_time >= duration:
-        last_action_time = t
-        actual_stimulus_num = (actual_stimulus_num + 1) % (len(stimulus_arr))
+try:
+    while True:
+        # Получаем текущее время
+        t = trialClock.getTime()
+        tThisFlip = window.getFutureFlipTime(clock=trialClock)
+        tThisFlipGlobal = window.getFutureFlipTime(clock=None)
+        frameN = frameN + 1  # Количество отрисованных кадров
+        # print(t, frameN)
+        if t - last_action_time >= duration:
+            last_action_time = t
+            actual_stimulus_num = (actual_stimulus_num + 1) % (len(stimulus_arr))
 
-    if type_show == 0:
-        for itt in range(0, len(stimulus_arr)):
-            if itt == actual_stimulus_num:
-                continue
-            stimulus_arr[itt].draw(window)
-    elif type_show == 1:
-        stimulus_arr[actual_stimulus_num].draw(window)
-    else:
-        for stimulus in stimulus_arr:
-            stimulus.draw(window)
-    if p300_info.check_flag():
-        print("Был P300! Время", t)
-        p300_info.drop_flag()
-        if type_show == 0 or type_show == 1:
-            if t - p300_delay < last_action_time:
-                back_count = ((last_action_time - t - p300_delay) // duration) + 1
-                print("Command is", number_to_direction(actual_stimulus_num - back_count) % len(stimulus_arr))
-            else:
-                print("Command is", number_to_direction(actual_stimulus_num))
-    window.flip()
-
+        if type_show == 0:
+            for itt in range(0, len(stimulus_arr)):
+                if itt == actual_stimulus_num:
+                    continue
+                stimulus_arr[itt].draw(window)
+        elif type_show == 1:
+            stimulus_arr[actual_stimulus_num].draw(window)
+        else:
+            for stimulus in stimulus_arr:
+                stimulus.draw(window)
+        if p300_info.check_flag():
+            print("Был P300! Время", t)
+            p300_info.drop_flag()
+            if type_show == 0 or type_show == 1:
+                if t - p300_delay < last_action_time:
+                    back_count = ((last_action_time - t - p300_delay) // duration) + 1
+                    print("Command is", number_to_direction((actual_stimulus_num - back_count) % len(stimulus_arr)))
+                else:
+                    print("Command is", number_to_direction(actual_stimulus_num))
+                ts = time.time()
+                print(ts)
+                f.write(str(ts) + '\n')
+        if defaultKeyboard.getKeys(keyList=["escape"]):
+            print("Was esc")
+            break
+        window.flip()
+except KeyboardInterrupt:
+    print('interrupted!')
+print("Out of cicle")
+f.close()
 window.flip()
+print("Out of after flip")
 window.close()
+print("Out of after close")
+core.quit()
+print("Out of after flip")
 
